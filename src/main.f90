714beb4b430a5b9b94cd884d8396a45d2374b87d
Brian Friesen <bfriesen@lbl.gov>
2016-10-21 14:53:58 -0700

    main: make array of initial conditions much bigger so we can get real times

--- src/main.f90
+++ src/main.f90
@@ -6,7 +6,7 @@ program main
     integer, parameter :: dp = kind(1.0d0)
 
     integer, parameter :: dp_simd_width = 4 ! AVX2 (Ivy Bridge) has 256 bit-wide SIMD width = 4 doubles
-    integer, parameter :: array_length = 8
+    integer, parameter :: array_length = 131072
     real(kind=dp) :: y0(array_length), t0, tf, dt0, eps
     real(kind=dp) :: t, dt, y(array_length)
     real(kind=dp) :: t1, t2
